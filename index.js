d6a554be6335e6246cbfaee3e8eec100a71d4d14
Matthew Stanciu <mattbstanciu@gmail.com>
2020-05-12 12:06:30 -0400

    Rewrite with Bolt

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
-const Botkit = require('botkit')
-const redisStorage = require('botkit-storage-redis')
+const { App } = require('@slack/bolt')
 
-const controller = new Botkit.slackbot({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  clientSigningSecret: process.env.SIGNING_SECRET,
-  scopes: ['bot', 'chat:write:bot'],
-  storage: redisStorage({ url: process.env.REDISCLOUD_URL })
-})
+const app = new App({
+	signingSecret: process.env.SIGNING_SECRET,
+	token: process.env.BOT_TOKEN
+});
 
-controller.spawn({ token: process.env.ACCESS_TOKEN })
-
-controller.setupWebserver(process.env.PORT || 3000, (err, webserver) => {
-  controller.createWebhookEndpoints(controller.webserver)
-  controller.createOauthEndpoints(controller.webserver)
-})
-
-controller.hears('', 'ambient', (bot, message) => {
-  const { ts, channel, text } = message.event
-
-  if (
-    !hasUrl(message.event.text) &&
-    message.event.thread_ts === undefined &&
-    (channel === 'CQPG0EUD8' || channel === 'C0M8PUPU6')
-  ) {
-    bot.api.chat.delete({ token: process.env.ACCESS_TOKEN, channel, ts })
-    bot.whisper(
-      message,
-      `Ahoy Matey! You posted a message without a file or URL:\n\n"${text}"\n\nI’ve removed your post for the time being, but you can repost a shipped project with a file or URL and I’ll let it be. Let <@U0C7B14Q3> know if you have any questions or if I made a mistake.`
-    )
-  }
-})
+app.event('message', async body => {
+	if (!hasUrl(body.event.text)
+		&& typeof body.event.thread_ts === 'undefined'
+		&& (body.event.channel === 'CQPG0EUD8' || body.event.channel === 'C0M8PUPU6')
+		&& body.event.subtype !== 'message_deleted') {
+		await app.client.chat.delete({
+			token: process.env.OAUTH_TOKEN,
+			channel: body.event.channel,
+			ts: body.event.event_ts
+		})
+		await app.client.chat.postEphemeral({
+			token: process.env.BOT_TOKEN,
+			attachments: [],
+			channel: body.event.channel,
+			text: `Ahoy Matey! You posted a message without a file or URL:\n\n"${body.event.text}"\n\nI’ve removed your message, but you can repost a shipped project with a file or URL and I’ll let it be. Let <@U4QAK9SRW> know if you have any questions or if I made a mistake.`,
+			user: body.event.user
+		})
+	}
+});
 
 const hasUrl = message => (
-  new RegExp(
-    '([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?'
-  ).test(message)
-)
+	new RegExp(
+		'([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?'
+	).test(message)
+);
+
+(async () => {
+	await app.start(process.env.PORT || 3000);
+	console.log("⚡️ Bolt app is running!");
+})();
